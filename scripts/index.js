f5c0e0ad7e20f5b8c0cebbc7d62e4a33067c5aa0
Nikolay Tegin <ntegin@yandex.ru>
2021-05-22 17:37:59 +0300

    fix: исправить рамку шапки наименование файла скриптов

--- scripts/index.js
+++ scripts/index.js
@@ -3,12 +3,11 @@ let formEdit = formElement.querySelector(".popup__form");
 let profileAddEdit = document.querySelector(".profile");
 let editButton = profileAddEdit.querySelector(".profile__edit-button");
 let closeButton = formElement.querySelector(".popup__close-button");
-let saveButton = formElement.querySelector(".popup__save-button");
 let popapProfileInfo = formElement.querySelectorAll(".popup__name");
 let nameInput = profileAddEdit.querySelector(".profile__title"); // Воспользуйтесь инструментом .querySelector()
 let jobInput = profileAddEdit.querySelector(".profile__subtitle"); // Воспользуйтесь инструментом .querySelector()
-function EditProfile() {
-  formEdit.firstName.value = nameInput.textContent;
+function editProfile() {
+  formEdit.author.value = nameInput.textContent;
   formEdit.interest.value = jobInput.textContent;
   formElement.classList.add("popup_opened");
 }
@@ -17,10 +16,10 @@ function closePopup() {
 }
 function formSubmitHandler(evt) {
   evt.preventDefault();
-  nameInput.textContent = formEdit.firstName.value;//передает имя из popup в input
+  nameInput.textContent = formEdit.author.value;//передает имя из popup в input
   jobInput.textContent = formEdit.interest.value;//передает деятельность из popup в input
   closePopup();// закрывает popup
 }
-editButton.addEventListener("click", EditProfile); 
+editButton.addEventListener("click", editProfile); 
 closeButton.addEventListener("click", closePopup);
 formEdit.addEventListener("submit", formSubmitHandler);
